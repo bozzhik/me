48e54e19c71117756c1a74c0e326753091ff8227
max <bozzhik@ya.ru>
2022-12-12 01:50:54 +0300

    *comments template

--- js/1.js
+++ js/1.js
@@ -1,1 +1,9 @@
-window.location.href = 'https://bozzhik.github.io/interactive/';
+window.location.href = 'https://bozzhik.github.io/interactive/'
+
+/**
+ * comments template
+ * * нужно глянуть как они адаптируются
+ * ! решить если их нужно изменять
+ * ? нужно ли их делать миксинами
+ * TODO: доделать грид сетку каталога и подборок
+ */
