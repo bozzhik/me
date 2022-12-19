44659cec8e146dea65058f61cae6303eabc9ee49
max <bozzhik@ya.ru>
2022-12-19 04:01:33 +0300

    *training.js 1.0

--- js/training.js
+++ js/training.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1')
 const h3 = document.querySelector('h3')
-const shortcuts = ['Ctrl+F5', 'Ctrl+F6', 'Ctrl+F7']
-const shortcutsUse = ['скопировать', 'вставить', 'вырезать']
+const shortcuts = ['Ctrl+F', 'Ctrl+A', 'Ctrl+C', 'Ctrl+V', 'Ctrl+S']
+const shortcutsUse = ['найти', 'выделить', 'скопировать', 'вставить', 'сохранить']
 
 let keyCombination = shortcuts[0]
 
@@ -10,35 +10,55 @@ h1.innerHTML = `${keyCombination}`
 document.addEventListener('keydown', function (event) {
    event.preventDefault()
 
-   if (event.ctrlKey && event.key === 'F5') {
-      h1.style.color = 'green'
-      h3.innerHTML = `Нажмите ${keyCombination} чтобы ${shortcutsUse[0]}`
+   if (event.ctrlKey && event.keyCode == 70) {
+      h1.style.color = '#cbfb45'
+      h3.innerHTML = `Нажмите <b>${keyCombination}</b> чтобы ${shortcutsUse[0]}`
 
       setTimeout(() => {
-         keyCombination = 'Ctrl+F6'
+         keyCombination = shortcuts[1]
          h1.innerHTML = `${keyCombination}`
          h3.innerHTML = ``
          h1.style.color = 'white'
-      }, 3000)
-   } else if (event.ctrlKey && event.key === 'F6') {
-      h1.style.color = 'green'
-      h3.innerHTML = `Нажмите ${keyCombination} чтобы ${shortcutsUse[1]}`
+      }, 2000)
+   } else if (event.ctrlKey && event.keyCode == 65) {
+      h1.style.color = '#cbfb45'
+      h3.innerHTML = `Нажмите <b>${keyCombination}</b> чтобы ${shortcutsUse[1]}`
 
       setTimeout(() => {
-         keyCombination = 'Ctrl+F7'
+         keyCombination = shortcuts[2]
          h1.innerHTML = `${keyCombination}`
          h3.innerHTML = ``
          h1.style.color = 'white'
-      }, 3000)
-   } else if (event.ctrlKey && event.key === 'F7') {
-      h1.style.color = 'green'
-      h3.innerHTML = `Нажмите ${keyCombination} чтобы ${shortcutsUse[2]}`
+      }, 2000)
+   } else if (event.ctrlKey && event.keyCode == 67) {
+      h1.style.color = '#cbfb45'
+      h3.innerHTML = `Нажмите <b>${keyCombination}</b> чтобы ${shortcutsUse[2]}`
 
       setTimeout(() => {
-         keyCombination = 'Ctrl+F8'
+         keyCombination = shortcuts[3]
          h1.innerHTML = `${keyCombination}`
          h3.innerHTML = ``
          h1.style.color = 'white'
-      }, 3000)
+      }, 2000)
+   } else if (event.ctrlKey && event.keyCode == 86) {
+      h1.style.color = '#cbfb45'
+      h3.innerHTML = `Нажмите <b>${keyCombination}</b> чтобы ${shortcutsUse[3]}`
+
+      setTimeout(() => {
+         keyCombination = shortcuts[4]
+         h1.innerHTML = `${keyCombination}`
+         h3.innerHTML = ``
+         h1.style.color = 'white'
+      }, 2000)
+   } else if (event.ctrlKey && event.keyCode == 83) {
+      h1.style.color = '#cbfb45'
+      h3.innerHTML = `Нажмите <b>${keyCombination}</b> чтобы ${shortcutsUse[4]}`
+
+      setTimeout(() => {
+         keyCombination = 'Пройдено!'
+         h1.innerHTML = `${keyCombination}`
+         h3.innerHTML = ``
+         h1.style.color = '#cbfb45'
+      }, 2000)
    }
 })
